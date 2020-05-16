4e325c69e77f64f820c70bf8891eb531c1ba7740
Banana_coder <ambadiritik@gmail.com>
2020-05-16 12:55:20 +0530

    Added Text and Title components ⭐️

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import PrimaryButton from './components/Buttons/PrimaryButton';
 import DangerButton from './components/Buttons/DangerButton';
 import NavBar from './components/NavBar/NavBar';
 import ProgressBar from './components/ProgressBars/ProgressBar';
+import Text from './components/Typography/Text';
+import Title from './components/Typography/Title';
 export default function App() {
 	return (
 		<Grid container direction="column">
@@ -17,6 +19,8 @@ export default function App() {
 					<PrimaryButton>Allocate</PrimaryButton>
 					<ProgressBar done={50} />
 					<ProgressBar done={90} />
+					<Text>This is some text</Text>
+					<Title>This is some title</Title>
 				</Grid>
 				<Grid item xs={false} sm={2}></Grid>
 			</Grid>
---
+++ src/assets/jss/typography.js
@@ -0,0 +1,13 @@
+const typography = {
+	text: {
+		fontFamily: 'var(--baseFont)',
+		color: 'var(--text-color)',
+	},
+	title: {
+		fontFamily: 'var(--baseFont)',
+		fontWeight: '600',
+		color: 'var(--bg-accent)',
+	},
+};
+
+export default typography;
